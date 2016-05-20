72c41c4820aba823ae0c36924b42b51e14ee2ab9
jsongcat <xianmaojohn8811@gmail.com>
2016-05-19 22:23:33 -0700

    build ProjectDetailProject

--- Don'tKillTheTree/app/src/main/java/dontkillthetree/scu/edu/UI/ProjectDetailActivity.java
+++ Don'tKillTheTree/app/src/main/java/dontkillthetree/scu/edu/UI/ProjectDetailActivity.java
@@ -1,13 +1,64 @@
 package dontkillthetree.scu.edu.UI;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ProjectDetailActivity extends ParentActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectDetailActivity extends ParentActivity implements AdapterView.OnItemClickListener {
+    private List<MilestoneInfo> milestoneInfoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_detail);
+
+        // get the widgets
+        EditText projectName = (EditText) findViewById(R.id.projectName);
+        EditText days = (EditText) findViewById(R.id.days);
+        EditText numberOfMilestone = (EditText) findViewById(R.id.numberOfMilestone);
+        EditText projectPartner = (EditText) findViewById(R.id.projectPartner);
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        // get data from db and create a arrayList for listView
+        milestoneInfoList = new ArrayList<>();
+
+        // set data to EditText
+
+        // create milestone list in run-time
+        listView.setAdapter(new MilestoneInfoArrayAdapter(this, R.layout.milestone_row, milestoneInfoList));
+        listView.setOnItemClickListener(this);
+
+        // get the save button
+        Button saveButton = (Button) findViewById(R.id.saveButton);
+        if (saveButton != null) {
+            saveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // add data to db
+                }
+            });
+        } else {
+            toastShow("fail to get the save button!");
+        }
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        // actually we don't need this one
+    }
+
+    private void toastShow(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
